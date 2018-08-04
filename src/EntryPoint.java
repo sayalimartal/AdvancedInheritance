3bc4c0fbf684b9fd87f70896d7f644ba66ea20d2
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-08-04 16:51:10 +0530

    Add files via upload

---
+++ src/EntryPoint.java
@@ -0,0 +1,37 @@
+//Entry point to test application
+public class EntryPoint {
+	public static void main(String[] args) {
+		
+		BankFactory mmBankFactory=new MMBankFactory();
+		
+		//Open a new salaried savings account
+		SavingAcc mmSavingAcc=mmBankFactory.getNewSavingAcc(100, "Sayali", 5000, true); 
+		//Open a new current account
+		CurrentAcc mmCurrentAcc=mmBankFactory.getNewCurrentAcc(101, "Siddhi", 3000, 10000);
+		
+		mmSavingAcc.withdraw(500);
+		System.out.println(mmSavingAcc);
+		System.out.println("******************************************");
+		
+		mmSavingAcc.withdraw(5000);
+		System.out.println(mmSavingAcc);
+		System.out.println("******************************************");
+		
+		mmCurrentAcc.withdraw(5000);
+		System.out.println(mmCurrentAcc);
+		System.out.println("******************************************");
+		
+		mmCurrentAcc.deposit(5000);
+		System.out.println(mmCurrentAcc);
+		System.out.println("******************************************");
+		
+		mmCurrentAcc.withdraw(20000);
+		System.out.println(mmCurrentAcc);
+		System.out.println("******************************************");
+		
+		//Open a new non-salaried savings account
+		SavingAcc mmSavingAcc2=mmBankFactory.getNewSavingAcc(102, "Sayali", 5000, false); 
+		mmSavingAcc2.withdraw(4500);
+		System.out.println(mmSavingAcc2);
+	}
+}
